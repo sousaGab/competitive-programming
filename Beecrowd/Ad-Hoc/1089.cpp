42658c3dd0ea9c37e559169249996c06c790fda4
sousaGab <gabrielamaralsousa@gmail.com>
2022-12-31 01:20:21 -0300

    update AdHoc

---
+++ Beecrowd/Ad-Hoc/1089.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define sc(a) scanf("%d", &a)
+
+using namespace std;
+int n, x, ans, arr[10000], nxt, pv;
+
+int main()
+{
+
+    while (sc(n) && n)
+    {
+        ans = 0;
+
+        for (int i = 1; i <= n; i++)
+            sc(arr[i]);
+
+        arr[0] = arr[n];
+        arr[n + 1] = arr[1];
+
+        for (int i = 1; i <= n; i++)
+        {
+            x = arr[i];
+            pv = arr[i - 1];
+            nxt = arr[i + 1];
+
+            if (nxt >= x && pv > x || nxt <= x && pv < x)
+                ans++;
+        }
+
+        printf("%d\n", ans);
+    }
+    return 0;
+}
