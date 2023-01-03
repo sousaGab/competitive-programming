8c4bfcef525af4c8e998c8adcac8746f22d0a3a4
sousaGab <gabrielamaralsousa@gmail.com>
2023-01-03 10:16:38 -0300

    problem 1564 solution

---
+++ Beecrowd/Beginner/1564.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    while (cin >> n)
+    {
+        if (n == 0)
+            printf("vai ter copa!\n");
+        else
+            printf("vai ter duas!\n");
+    }
+    return 0;
+}
